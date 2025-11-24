8eb9bc7ca1dfa1fe982463a6fab7dd8e83c2000f
Solal Pirelli <solal.pirelli@gmail.com>
2025-11-24 12:30:59 +0100

    Améliorations du script pour conversion en PDF, manque plus que les caractères non-latins

--- scripts/pdf/ReadMe.md
+++ scripts/pdf/ReadMe.md
@@ -7,4 +7,3 @@ Leur ordre est défini en dur dans le script, à changer donc si l'ordre change.
 **Bugs actuels**:
 - Les caractères chinois et arabes (c.f. fin de la page 2 dans l'intro par exemple) ne sont pas affichés
   - Il est possible de donner à pandoc un nom de police à utiliser, mais cela cause des problèmes de chargement d'une police japonaise...
-- Le texte dans les images SVG ne s'affiche pas
--- scripts/pdf/markdown-to-pdf.sh
+++ scripts/pdf/markdown-to-pdf.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
 
-if ! git status '../../cours' | grep -q 'nothing to commit, working tree clean'; then
+if ! git status '../../cours' '../../ReadMe.md' | grep -q 'nothing to commit, working tree clean'; then
   echo 'Veuillez lancer ce script uniquement quand les fichiers du cours ne sont pas modifiés... désolé.'
   exit 1
+fi
+
+if git clean -xfdn '../../cours' | grep -q 'Would remove'; then
+  echo 'Veuillez lancer ce script uniquement quand les fichiers du cours ne sont pas modifiés... désolé (Utilisez git clean).'
+  exit 2
+fi
+
+if ! which rsvg-convert >'/dev/null'; then
+  echo 'Veuillez installer rsvg-convert (package librsvg2-bin sur Ubuntu par exemple).'
+  exit 3
+fi
+
+if ! docker version >'/dev/null'; then
+  echo 'Veuillez installer et lancer Docker.'
+  exit 4
 fi
 
 # On pourrait utiliser `ls` mais on préfère avoir les cours dans l'ordre.
@@ -19,10 +34,24 @@ GUIs-et-APIs
 EOF
 )"
 
+# Commençons par créer une intro en enlevant la table des matières
+sed -i '/## Contenu/,$d' '../../ReadMe.md'
+sed -i "s|](documentation|](https://github.com/sweng-epfl/turing/tree/main/documentation|g" '../../ReadMe.md'
+
 # Le markdown du cours utilise du HTML pour afficher les images à une échelle raisonnable.
 # `pandoc` ne supporte pas ça en combinaison avec `rebase_relative_paths` (seuls les images Markdown sont supportées par cette extension).
 # Donc on les convertit, adieu l'échelle...
 sed -i 's|<p align="center"><img alt="\(.*\)" src="\(.*\)" width=".*" /></p>|![\1](\2)|g' '../../cours/'*'/ReadMe.md'
+
+# Pandoc gère mal les SVGs, c.f. https://github.com/jgm/pandoc/issues/8176
+# Donc on les transforme en PDF à l'avance
+for c in $order; do
+  for i in "../../cours/$c/images/"*.svg; do
+    [ -f "$i" ] || break # ignorer les dossiers dans lesquels il n'y a pas de SVG
+    rsvg-convert -f 'pdf' --dpi-x '300' --dpi-y '300' -o "$i.pdf" "$i"
+  done
+  sed -i 's|.svg)|.svg.pdf)|g' "../../cours/$c/ReadMe.md"
+done
 
 # On veut aussi que les liens vers les exercices soient absolus
 for c in $order; do
@@ -39,11 +68,12 @@ docker run --rm \
        --volume "$(pwd)/../..:/data" \
        --user "$(id -u):$(id -g)" \
        'pandoc/extra:3.8.2.1' \
-       $(echo "$order" | sed 's|.*|cours/&/ReadMe.md|') \
+       'ReadMe.md'  $(echo "$order" | sed 's|.*|cours/&/ReadMe.md|') \
        --pdf-engine 'lualatex' \
        --from 'gfm-yaml_metadata_block+rebase_relative_paths' \
        -V 'geometry:margin=1in' \
        -V 'colorlinks=true' \
        -o 'scripts/pdf/GénieLogiciel.pdf'
 
-git restore '../../cours'
+git restore '../../cours' '../../ReadMe.md'
+git clean -xfd '../../cours' >'/dev/null'
