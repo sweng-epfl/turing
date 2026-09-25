7775fa7c422fbac3d313803df6e68d9fdc261ab7
Solal Pirelli <solal.pirelli@gmail.com>
2026-09-25 16:46:00 +0200

    Amélioration des exercices de débuggage

--- cours/Débuggage/exercices/cours/binarytree.py
+++ cours/Débuggage/exercices/cours/binarytree.py
@@ -1,16 +1,36 @@
 # Exercise: Ce code a plusieurs bugs, trouvez-le en exécutant le code et corrigez-les à l'aide d'un debugger
-#           Par exemple, utilisez la fonction python `breakpoint()` pour entrer dans le debugger `pdb`
-
-from typing import List, Optional
+#           Par exemple, utilisez Visual Studio Code.
+#           (ou, en ligne de commande, utilisez la fonction Python `breakpoint()` pour entrer dans le debugger `pdb`)
 
 class BinaryTree:
-    def __init__(self, v: str, l: Optional['BinaryTree'], r: Optional['BinaryTree']):
+    """
+    Représente un arbre binaire. Chaque noeud peut avoir un descendant à gauche et un descendant à droite,
+    ou `None` pour les deux si le noeud est une "feuille" de l'arbre.
+
+    Par exemple:
+          a
+        /   \
+       b     c
+      / \   / \
+     d   e f   g
+    """
+    def __init__(self, v, l, r):
+        """
+        Initialise un arbre binaire avec valeur 'v' et descendants 'l' (gauche) et 'd' (droite).
+        """
         self.value = v
         self.left = l
         self.right = l
 
+    # Une méthode annotée par `@staticmethod` ne prend pas de 'self',
+    # et s'appelle sur la classe directement, donc `BinaryTree.from_list([...])` dans ce cas.
     @staticmethod
-    def from_list(lst: List[str]) -> Optional['BinaryTree']:
+    def from_list(lst):
+        """
+        Crée un arbre binaire depuis une liste. La valeur est au milieu, le descendant à gauche est la moitié gauche, etc.
+        Par exemple, l'arbre donné en exemple dans la documentation de cette classe est obtenu depuis
+        ['d', 'b', 'e', 'a', 'f', 'c', 'g']
+        """
         mid = len(lst) // 2
         return BinaryTree(
             lst[mid],
@@ -18,7 +38,10 @@ class BinaryTree:
             BinaryTree.from_list(lst[mid:len(lst)-1])
         )
 
-    def to_list(self) -> List[str]:
+    def to_list(self):
+        """
+        Convertit cet arbre en liste, dans le format attendu par `from_list`.
+        """
         result = []
         if self.left is not None:
             result.extend(self.left.to_list())
@@ -27,6 +50,10 @@ class BinaryTree:
             result.extend(self.right.to_list())
         return result
 
+    # La méthode spéciale `__str__` est utilisée quand Python doit convertir un objet en chaîne de caractères
+    # (par exemple, 'str(self.left)' ici)
+    def __str__(self):
+        return "Tree(" + str(self.left) + ", " + self.value + ", " + str(self.right) + ")"
 
 def main():
     lst = ["ABC", "DEF", "GHI", "JKL", "MNO", "PQR", "STU"]
@@ -34,6 +61,5 @@ def main():
     new_list = tree.to_list()
     print("Liste avant : " + ", ".join(lst))
     print("Liste après : " + ", ".join(new_list))
-
 
 main()
--- cours/Débuggage/exercices/cours/stack.py
+++ cours/Débuggage/exercices/cours/stack.py
@@ -1,31 +1,35 @@
 # Exercice: Ajoutez du code à IntStack pour attrapper les bugs tôt et corrigez les bugs que vous trouvez.
 #           Gardez à l'esprit que les utilisateurs d'IntStack doivent pouvoir tester les préconditions eux-mêmes.
 #           Ajoutez des méthodes publiques ou privées si nécessaire.
-
-from typing import Optional
+#           Étape 1 : ajoutez la précondition nécessaire au constructeur. (Qu'est-il interdit/insensé ?)
+#           Étape 2 : ajoutez une méthode 'def assert_invariants(self)' qui vérifie les invariants de la classe,
+#                     et appelez-la (1) à la fin du constructeur, (2) au début de chaque méthode, et (3) à la fin de chaque méthode
+#           Étape 3 : Corrigez les bugs !
 
 class IntStack:
     """Une pile d'entiers, avec une taille maximale."""
-    def __init__(self, max_size: int):
+    def __init__(self, max_size):
         """Crée une pile vide avec la taille maximale donnée ; il est interdit d'empiler plus d'éléments que cette taille."""
         self.top = -1
         self.values = [0] * max_size
 
-    def pop(self) -> Optional[int]:
-        """Retourne et enlève de la pile l'élément sur le dessus de la pile, ou retourne null si la pile est vide."""
+    def pop(self):
+        """Retourne et enlève de la pile l'élément sur le dessus de la pile, ou retourne None si la pile est vide."""
         value = self.values[self.top] if self.top >= 0 else None
         self.top -= 1
         return value
 
-    def push(self, value: int):
+    def push(self, value):
         """Empile la valeur donnée."""
         self.values[self.top] = value
         self.top += 1
 
 # exemple d'utilisation d'IntStack
-def use_stack(stack: IntStack):
+def main():
+    stack = IntStack(2)
     stack.push(1)
-    stack.pop()
-    stack.pop()
+    assert stack.pop() == 1
+    assert stack.pop() == None
+    assert stack.pop() == None
 
-use_stack(IntStack(4))
+main()
